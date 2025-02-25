051b14328e153bd0e855c0703ab929c84c8879d7
Deepali <deepalisingh3233@gmail.com>
2025-02-25 13:30:22 +0530

    About Math and its methods

---
+++ JavaScript/maths.js
@@ -0,0 +1,29 @@
+//? Maths work with Numbers. Math doesn't works on BigInt
+
+// const piValue = Math.PI;
+// console.log(piValue);
+
+// console.log(Math.abs(5));
+// console.log(Math.abs(-5));
+
+// const roundValue = Math.round(3.7)
+// console.log(roundValue);
+
+// console.log(Math.ceil(3.7));
+// console.log(Math.floor(3.7));
+
+// console.log(Math.trunc(3.2));
+
+// console.log(Math.trunc(-3.2));
+// console.log(Math.floor(-3.2));
+
+// console.log(Math.pow(2, 4));
+// console.log(2 ** 3);
+
+// console.log(Math.sqrt(25));
+// console.log(Math.log(2));
+// console.log(Math.log2(8));
+
+
+console.log(Math.round(Math.random() * 100));
+console.log((Math.random() * 100).toFixed(3));
